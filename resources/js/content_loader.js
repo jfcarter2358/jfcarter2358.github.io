2ebf4b52006205c1c324cc367a51d94605d42aab
John Carter <jfcarter2358@gmail.com>
2019-01-23 13:44:41 -0600

    Built out all pages and populated with preliminary content

---
+++ resources/js/content_loader.js
@@ -0,0 +1,428 @@
+var pageContent = {
+    "boldinsight" : 
+    {
+        "title" : "Bold Insight",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Alexa Application",
+                "content" : 
+                [
+                    "Developed an application to provide a new experience to users and push the bounds of the UX Research field through an innovative approach to user interaction"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Large-scale Healthcare Data Analysis",
+                "content" : 
+                [
+                    "Created a large-scale database of historical health data with over 7,000,000 records",
+                    "Developed a user interface to allow for the exploration and manipulation of data",
+                    "Leveraged machine learning algorithms to perform analysis on the data and provide insight into correlations with external events and factors"
+                ]
+            }
+        }
+    },
+    "clinecenter" : 
+    {
+        "title" : "Cline Center for Advanced Social Research",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Political Conflict Event Extraction and Analysis",
+                "content" : 
+                [
+                    "Developed a MongoDB/Python based pipeline for automated political conflict event extraction from news text using natural language processing",
+                    "Contributed to team effort resulting in publication of several million political conflict events extracted from over 14 million news articles spanning 1945 – 2015 generated from corpora composed of articles from BBC Monitoring's Summary of World Broadcasts, CIA's Foreign Broadcast Information Service, and the New York Times"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Textual Location Extraction and Focus Identification",
+                "content" : 
+                [
+                    "Created software for the extraction of locations from within textual resources and extrapolating the “focus” country of a news article",
+                    "Improved accuracy over industry geolocation standard by up to 56.3% with initial investigation suggesting a significant increase in performance regarding comparable tools developed by labs at Penn State and MIT"
+                ]
+            },
+            "b2" : 
+            {
+                "title" : "Relevant Political Actor Extraction",
+                "content" : 
+                [
+                    "Developed intrastate relationship extraction software to extract relevant political actors from corpora surrounding the onset of hostilities"
+                ]
+            },
+            "b3" : 
+            {
+                "title" : "International Collaboration",
+                "content" : 
+                [
+                    "Participated in the multinational conference, Collaborative Research on Extreme Scale Text Analytics (CRESTA), at the Cline Center for Advanced Social Research, furthering research regarding international political conflict",
+                    "Produced large volumes of data utilized by research teams to further computational political science research"
+                ]
+            },
+            "b4" : 
+            {
+                "title" : "Software Developer Effort Metric",
+                "content" : 
+                [
+                    "Developed a methodology for defining the ‘effort required for upkeep and the addition of new components’ into an existing software pipeline"
+                ]
+            }
+        }
+    },
+    "harvard" : 
+    {
+        "title" : "Harvard University Department of Government",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Computational Political Science Research",
+                "content" : 
+                [
+                    "Implementing and developing natural language processing techniques to model media coverage of geopolitical events",
+                    "Performing analysis of large-scale datasets to extract meaningful relationships between political entities involved in relevant time periods"
+                ]
+            }
+        }
+    },
+    "multiverse" : 
+    {
+        "title" : "Multiverse Game Studios",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Game Engine Development",
+                "content" : 
+                [
+                    "Created DirectX11 graphics engine from scratch to handle the demands of development projects and provide functionality not present in existing engines",
+                    "Built custom physics engine to handle collision between in-game objects as well as provide a proper physical response to provide a realistic experience"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Engine Performance Research",
+                "content" : 
+                [
+                    "Conducting research to push past industry standards by offloading physics and collisions computations onto the GPU to reduce the CPU load"
+                ]
+            }
+        }
+    },
+    "gfactor" : 
+    {
+        "title" : "Determination of Muon g-factor from Cosmic Rays",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Description",
+                "content" : 
+                [
+                    "Finished construction of the cosmic ray detector",
+                    "Created logic setup to eliminate false counts from the scintillator/photomultiplier detectors",
+                    "Determined cosmic ray muon g-factor to within 2 sigma of theoretical results",
+                    "Performed analysis of muon detection time and angle dependence to confirm that cosmic rays are of extra-solar origin"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Report",
+                "content" : []
+            }
+        }
+    },
+    "opticalpumping" :
+    {
+        "title" : "Optical Pumping with Rb<sup>85</sup> and Rb<sup>87</sup>",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Description",
+                "content" : 
+                [
+                    "Setup apparatus to nullify the effects of the Earth’s magnetic field to the maximum degree possible",
+                    "Measured the resonant frequency of each isotope in a known magnetic field to determine each isotope’s spin (g<sub>f</sub>) value",
+                    "Swept the horizontal magnetic field in the apparatus to determine the isotope’s Zeeman resonance"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Report",
+                "content" : []
+            }
+        }
+    },
+    "opticalpumping" :
+    {
+        "title" : "Time Domain Properties of Laser-induced Ruby Fluorescence",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Description",
+                "content" : 
+                [
+                    "Measured exponential decay of ruby fluorescence lifetime",
+                    "Compared experimental data to theoretical single and double lifetime curves",
+                    "Measured change in exponential decay with sample temperature"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Report",
+                "content" : []
+            }
+        }
+    },
+    "opticalpumping" :
+    {
+        "title" : "Ferroelectric Hysteresis Properties of BaTiO<sub>3</sub> and Curie Temperature Determination",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Description",
+                "content" : 
+                [
+                    "Prepared sample of BaTiO<sub>3</sub> for experimentation",
+                    "Methodically tested the sample across a large temperature domain"
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Report",
+                "content" : []
+            }
+        }
+    },
+    "opticalpumping" :
+    {
+        "title" : "Pulsed Nuclear Magnetic Resonance in Water with FeCl<sub>3</sub>",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Description",
+                "content" : 
+                [
+                    "Prepared multiple sample solutions with different FeCl<sub>3</sub> concentrations",
+                    "Systematically acquired data using a PNMR spectrometer "
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "Report",
+                "content" : []
+            }
+        }
+    },
+    "datasciencesummit.html" :
+    {
+        "title" : "Illinois Data Science Initiative (iDSI) Industry Data Science Summit Poster Presentation",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Presentation Poster",
+                "content" : 
+                [
+                    ""
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "",
+                "content" : 
+                [
+                    "Presented a poster representation of the 2018 Undergraduate Research Symposium Oral Presentation"
+                ]
+            }
+        }
+    },
+    "undergradsymposiumoral" :
+    {
+        "title" : "UIUC 2018 Undergraduate Research Symposium Oral Presentation",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Presentation Slides",
+                "content" : [],
+            },
+            "b1" : 
+            {
+                "title" : "",
+                "content" : 
+                [
+                    "Presented research conducted on extracting relevant pollical actors in intra-state conflicts to provide a method of determining the most effective diplomatic resource allocation to prevent the breakout of armed hostilities"
+                ]
+            }
+        }
+    },
+    "undergradsymposiumposter" :
+    {
+        "title" : "UIUC 2017 Undergraduate Research Symposium Poster Presentation",
+        "blocks" : 
+        {
+            "b0" : 
+            {
+                "title" : "Presentation Poster",
+                "content" : 
+                [
+                    ""
+                ]
+            },
+            "b1" : 
+            {
+                "title" : "",
+                "content" : 
+                [
+                    "Presented research on large-scale political conflict event extraction from massive-scale news article corpora and improvements on existing algorithms",
+                    "Presented research on the generation of a novel article focal country extraction algorithm that outperformed existing algorithms"
+                ]
+            }
+        }
+    },
+    "eventdata" : 
+    {
+        "title" : "Cline Center Historical Event Data Publication",
+        "blocks" :
+        {
+            "b0" :
+            {
+                "title" : "Description",
+                "content" : 
+                [
+                    "Data publication by the Cline Center for Advanced Social Research that documents conflict issues, events, and involved agents spanning from 1945 - 2015. The data was produced using a modified and improved version of the Open Event Data Alliance's PETRARCH-2 automated event coding software. The developed pipeline processed 14 million news articles to produce several million conflict events"
+                ]
+            },
+            "b1" :
+            {
+                "title" : "Citation",
+                "content" : 
+                [
+                    "Althaus, Scott, Joseph Bajjalieh, John F. Carter, Buddy Peyton, and Dan A. Shalmon. 2017. Cline Center Historical Phoenix Event Data. v.1.0.0. Distributed by Cline Center for Advanced Social Research. June 30. <a href=\"http://www.clinecenter.illinois.edu/data/event/phoenix/\">http://www.clinecenter.illinois.edu/data/event/phoenix/.</a>"
+                ]
+            }
+        }
+    },
+    "uiuc":
+    {
+        "title" : "Education",
+        "blocks" :
+        {
+            "b0" : 
+            {
+                "title" : "University of Illinois at Urbana-Champaign",
+                "content" : 
+                [
+                    "B.S. Physics, concentration Astrophysics",
+                    "Minor Informatics"
+                ],
+            },
+            "b1" :
+            {
+                "title" : "Astronomy Courses",
+                "content" :
+                [
+                    "<div><p style='font-family:monospace;display:inline;'>ASTR 210 --&nbsp;</p>Introduction to Astrophysics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>ASTR 404 --&nbsp;</p>Stellar Astrophysics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>ASTR 414 --&nbsp;</p>Astronomical Techniques</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>ASTR 496 --&nbsp;</p>Seminar in Astronomy</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>ASTR 596 --&nbsp;</p>Machine Learning</div>",
+                ]
+            },
+            "b2" : 
+            {
+                "title" : "Informatics Courses",
+                "content" : 
+                [
+                    "<div><p style='font-family:monospace;display:inline;'>INFO 202 --&nbsp;</p>Social Aspects Info Tech</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>INFO 490 --&nbsp;</p>Advanced Data Science</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>INFO 490 --&nbsp;</p>Introduction to Data Science</div>",
+                ]
+            },
+            "b3" :
+            {
+                "title" : "Linguistics Courses",
+                "content" :
+                [
+                    "<div><p style='font-family:monospace;display:inline;'>LING 406 --&nbsp;</p>Intro to Computational Linguistics</div>",
+                ]
+            },
+            "b4" : 
+            {
+                "title" : "Math Coursers",
+                "content" : 
+                [
+                    "<div><p style='font-family:monospace;display:inline;'>MATH 231 --&nbsp;</p>Calculus II</div></div>",
+                    "<div><p style='font-family:monospace;display:inline;'>MATH 241 --&nbsp;</p>Calculus III</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>MATH 285 --&nbsp;</p>Differential Equations</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>MATH 402 --&nbsp;</p>Non-Euclidean Geometry</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>MATH 415 --&nbsp;</p>Applied Linear Algebra</div>",
+                ]
+            },
+            "b5" : 
+            {
+                "title" : "Physics Courses",
+                "content" : 
+                [
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 199 --&nbsp;</p>An Introduction to Computing in Physics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 211 --&nbsp;</p>University Physics: Mechanics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 212 --&nbsp;</p>University Physics: Electricity and Magnetism</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 213 --&nbsp;</p>University Physics: Thermal Physics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 214 --&nbsp;</p>University Physics: Quantum Mechanics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 225 --&nbsp;</p>Relativity and Math Applications</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 325 --&nbsp;</p>Classical Mechanics I</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 401 --&nbsp;</p>Classical Physics Lab</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 403 --&nbsp;</p>Modern Experimental Physics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 427 --&nbsp;</p>Thermal and Statistical Physics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 435 --&nbsp;</p>Electromagnetic Fields I</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 475 --&nbsp;</p>Introduction to Biophysics</div>",
+                    "<div><p style='font-family:monospace;display:inline;'>PHYS 486 --&nbsp;</p>Quantum Physics I</div>",
+                ]
+            }
+        }
+    }
+};
+
+$(document).ready(
+    function()
+    {
+        // get current page name
+        var urlParts = $(location).attr("href").split("/");
+        var pageName = urlParts.pop().split(".")[0];
+
+        // define variables
+        var contentTitle = document.getElementById("content-title");
+        var contentPanel = document.getElementById("content-panel");
+
+        var content = "";
+
+        for (var b in pageContent[pageName]["blocks"])
+        {
+            // add in section title as header
+            content += "<h4>" + pageContent[pageName]["blocks"][b]["title"] + "</h4>";
+            // create section content list and populate it
+            content += "<ul>";
+            for (var c in pageContent[pageName]["blocks"][b]["content"])
+            {
+                content += "<li>" + pageContent[pageName]["blocks"][b]["content"][c] + "</li>";
+            }
+            content += "</ul>";
+            // add spacing between sections
+            content += "<br>";
+        }
+
+        // populate content and title elements with data
+        contentTitle.innerHTML = "<h3>" + pageContent[pageName]["title"] + "</h3>";
+        contentPanel.innerHTML = content;
+    }
+);
